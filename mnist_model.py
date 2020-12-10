28d9ca648e0ad203519442b935ba5d0d22d12fb2
axcyoung <alphacyoung@gmail.com>
2020-12-09 20:26:00 -0800

    Update mnist_model.py

--- mnist_model.py
+++ mnist_model.py
@@ -8,34 +8,25 @@ class MNIST_Model(tf.keras.Model):
         self.num_classes = 10
         self.batch_size = 100
         
-        # initialize trainable parameters
-
+        # initialize trainable parameters 
         if params == None:
-            self.fc1 = tf.zeros((self.fc1_unit,), name = 'fc1')
-            self.fc2 = tf.zeros((self.num_classes,), name = 'fc2')
+            self.fc1 = tf.random.truncated_normal([784, self.fc1_unit], stddev=.1, dtype=tf.float32, name='fc1')
+            self.fc2 = tf.random.truncated_normal([self.fc1_unit, self.num_classes], stddev=.1, dtype=tf.float32, name='fc2')
         else:
             self.fc1 = params['fc1']
             self.fc2 = params['fc2']
-        
-        #self.fc1 = tf.keras.layers.Dense(
-        #    self.fc1_unit, 
-        #    activation = 'relu'
-        #)
-        #self.fc2 = tf.keras.layers.Dense(
-        #    self.num_classes, 
-        #    activation = 'softmax' 
-        #)
 
         return  
-
-    def get_params(self):
-        return [('theta', self.theta)]
+        
+    def get_params(self): 
+        return [('fc1', self.fc1), ('fc2', self.fc2)]
 
     def update_params(self, change_tensors): 
         '''
         :param change_tensors: a list containing just one tensor, which is to be added to self.theta
         '''
-        self.theta += change_tensors['theta']
+        self.fc1 += change_tensors['fc1']
+        self.fc2 += change_tensors['fc2']
 
     def call(self, input): 
         '''
@@ -43,12 +34,13 @@ class MNIST_Model(tf.keras.Model):
         :param input: shape = (batch_sz, input_sz)
         :return the probabilities of each label, shape = (batch_sz, num_classes)
         '''
-        fc1_output = self.fc1(input) 
-        fc2_output = self.fc2(fc1_output) 
-
-        prbs = fc2_output 
-
-        return prbs
+        fc1_output = tf.linalg.matmul(input, self.fc1)
+        fc1_output = tf.nn.relu(fc1_output)
+        fc2_output = tf.linalg.matmul(fc1_output, self.fc2)
+        fc2_output = tf.nn.softmax(fc2_output)
+        print(fc2_output)
+        
+        return fc2_output
 
     def loss_function(self, predictions, labels): 
         '''
